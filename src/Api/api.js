eb4d66567aa996c2bc40627835795aefdbba5dd6
kowalczyk-mateusz <kowalczyk_mateusz@poczta.fm>
2021-01-19 22:27:05 +0100

    first setup

---
+++ src/Api/api.js
@@ -0,0 +1,3 @@
+const baseUrl = `https://api.themoviedb.org/3/authentication/token/new?api_key=${apiKey}`
+const apiKey = `7d01e2f21937769f340b6be2e4f1f146`;
+const popularMovies = `https://api.themoviedb.org/3/movie/popular?api_key=7d01e2f21937769f340b6be2e4f1f146&page=1`
--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect, useState} from 'react'
+import axios from 'axios'
 
 function App() {
+  const [movie, setMovie] = useState(null);
+  useEffect(()=>{
+  axios.get('https://api.themoviedb.org/3/movie/popular?api_key=7d01e2f21937769f340b6be2e4f1f146&page=1')
+  .then (data =>{
+    setMovie(data.data)
+    console.log(data.data)
+    
+  })
+  .catch(error => console.log(error))
+}, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {movie &&(
+        <div>
+          {movie.results.map(wynik=>(
+            <h1>
+              {wynik.original_title}
+            </h1>
+          ))}
+        </div>
+        )}
     </div>
   );
 }
